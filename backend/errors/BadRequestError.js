f070f9edad7308c393bacdec0ade8e0a2f808964
AntonMikhailov1 <mikhailovanton.nsk@gmail.com>
2023-10-08 20:30:51 +0300

    fix

--- backend/errors/BadRequestError.js
+++ backend/errors/BadRequestError.js
@@ -1,7 +1,7 @@
 const httpStatus = require('http-status-codes').StatusCodes;
 
 class BadRequestError extends Error {
-  constructor({ message }) {
+  constructor(message) {
     super(message);
     this.status = httpStatus.BAD_REQUEST;
   }
